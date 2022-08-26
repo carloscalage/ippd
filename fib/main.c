ff03efb678f51d92d514764c2e1a0573f7ff7314
Carlos Augusto Calage <calage.carlos@gmail.com>
2022-08-26 11:26:19 -0300

    feat: added sequential fibonacci

--- fib/main.c
+++ fib/main.c
@@ -1,38 +1,60 @@
 #include <stdio.h>
 #include "omp.h"
 
-int fib(int n) {
+int fib(int n, int type) {
   int i, j;
 
   if (n < 2) {
     return n;
   }
   else {
-    #pragma omp task shared(i)
-    i=fib(n-1);
 
-    #pragma omp task shared(j)
-    j=fib(n-2);
+    if (type == 0) {
+      i = fib(n - 1, 0);
+      j = fib(n - 2, 0);
+    }
+    else {
+      #pragma omp task shared(i)
+      i = fib(n - 1, 1);
 
-    #pragma omp taskwait
+      #pragma omp task shared(j)
+      j = fib(n - 2, 1);
+
+      #pragma omp taskwait
+    }
+
     return i+j;
    }
 }
 
-int main () {
+int main (int argc, char *argv[]) {
   int begin = omp_get_wtime();
   int result;
+  int n = atoi(argv[1]);
+
+  /* Parallel */
 
   #pragma omp parallel 
   {
     #pragma omp single
-    result = fib(40);
+    result = fib(n, 1);
   } 
 
   int end = omp_get_wtime();
   
   printf("Result: %d\n", result);
-  printf("Time: %d\n", end - begin);
+  printf("Time: %ds\n", end - begin);
+
+  /* Sequential */
+
+  begin = omp_get_wtime();
+
+  result = fib(n, 0);
+
+  end = omp_get_wtime();
+
+  printf("Result: %d\n", result);
+  printf("Time: %ds\n", end - begin);
 
   return 0;
 }
